64b6ba891b250a9107ece8f7a508a99f312ff191
000ashishYadav <000ashishyadav2003@gmail.com>
2025-02-25 16:45:26 +0530

    files path changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./html/first.html"],
+  content: ["./first.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
